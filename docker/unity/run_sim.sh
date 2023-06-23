7e681a6e3552b5641a495aac1ddbd4ff8b406d88
Takanori Ugai <ugai@fujitsu.com>
2023-06-23 13:48:24 +0900

    VH version up

--- docker/unity/run_sim.sh
+++ docker/unity/run_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 640 480 Beautiful
 xvfb-run --auto-servernum --server-args="-screen 0 640x480x24" \
-    /unity_vol/executable_unix/Build_2023_0111.x86_64 \
+    /unity_vol/executable_unix/Modified_Camera_Build_Linux.x86_64 \
     -http-port=8080
     #-screen-width=$1 -screen-height=$2 -screen-quality=$3 \
